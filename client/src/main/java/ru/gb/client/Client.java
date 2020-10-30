ad260681dd2a5f5fbb5e29e5be208a3b758ced2b
Dmitry Lytov <dmitrycmc@gmail.com>
2020-10-30 20:33:11 +0300

    Promblem

--- client/src/main/java/ru/gb/client/Client.java
+++ client/src/main/java/ru/gb/client/Client.java
@@ -1,5 +1,6 @@
 package ru.gb.client;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 public class Client {
@@ -7,10 +8,15 @@ public class Client {
         Network network = new Network("localhost", 8189);
         network.start();
 
-        network.send(Paths.get("client_storage/1.jpg"));
-        network.send(Paths.get("client_storage/2.jpg"));
-        network.send(Paths.get("client_storage/3.bmp"));
-        network.send(Paths.get("client_storage/4.bmp"));
-        network.waitForAnswer();
+//        network.send(Paths.get("client_storage/1.jpg"));
+//        network.send(Paths.get("client_storage/2.jpg"));
+//        network.send(Paths.get("client_storage/3.bmp"));
+//        network.send(Paths.get("client_storage/4.bmp"));
+        try {
+            network.out.write(new byte[] {1}); // байт отправляется
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        network.waitForAnswer(); // байт не приходит
     }
 }
